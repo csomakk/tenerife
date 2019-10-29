d19d90b4c8a33f3f1643922286c006867512bf4e
Csomak Gabor <csomakk@gmail.com>
2019-10-29 20:48:22 +0100

    init

--- src/layouting/factories/DefaultFactory.ts
+++
@@ -1,8 +0,0 @@
-import * as PIXI from 'pixi.js';
-
-export class DefaultFactory {
-    public create(layout:any) {
-        let displayObject:PIXI.Container = new PIXI.Container();
-        return displayObject;
-    }
-}
--- src/layouting/factories/ImageFactory.ts
+++
@@ -1,9 +0,0 @@
-import * as PIXI from 'pixi.js';
-
-export class ImageFactory {
-    public create(layout:any) {
-        let pathPre = "default/generic/scale-2/"
-        let sprite = PIXI.Sprite.from(pathPre + layout["imageSource"]);
-        return sprite;
-    }
-}
