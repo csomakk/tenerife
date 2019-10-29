934c2b3b00eeaab74bef8c7a36bcbdb9b49fa89c
Csomak Gabor <csomakk@gmail.com>
2019-10-29 20:53:19 +0100

    init for conference

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import { PixiAppManager } from './pixi/pixiAppManager';
-import { LayoutFactory } from './layouting/LayoutFactory';
 
 var appManager = new PixiAppManager();
 appManager.createApp();
-
-var layoutFactory = new LayoutFactory();
-layoutFactory.buildLayoutFromRoot(appManager.app.stage);
