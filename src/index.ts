1b68b1b761d60d2338027dc74ff78d56b3d4051c
Csomak Gabor <csomakk@gmail.com>
2019-10-29 21:09:10 +0100

    BUNNY 2

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { PixiAppManager } from './pixi/pixiAppManager';
 var appManager = new PixiAppManager();
 appManager.createApp();
 
-const bunny = PIXI.Sprite.from('/bunny.png');
+const bunny = PIXI.Sprite.from('bunny.png');
 appManager.app.stage.addChild(bunny);
