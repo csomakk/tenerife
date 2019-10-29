3b1162cd675980495d016f1fd5377e8242657cc1
Csomak Gabor <csomakk@gmail.com>
2019-10-29 21:23:39 +0100

    rotate

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,26 @@ fetch("/magic.json")
     .then(body => {
     console.log('weee');
     body.childs.forEach((element:any) => {
-        var newItem;
+        let newItem:PIXI.DisplayObject;
         if (element.type == "sprite") {
             newItem = PIXI.Sprite.from(element.textureUrl);
         }
-        newItem.position = element.position;
-
+        if (element.position != null) {
+            newItem.position = element.position;
+        }
+        if (element.position != null) {
+            newItem.name = element.name;
+        }
+        if (element.onTick != null) {
+            appManager.app.ticker.add((delta) => {
+                console.log('tick');
+                if (element.onTick.rotate != null) {
+                    newItem.rotation += element.onTick.rotate * delta;
+                }
+               
+            });
+        }
+        
         appManager.app.stage.addChild(newItem);
     });
 });
